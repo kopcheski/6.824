6e5f17ac433edce7d19b021f0ef16c9f75121e8d
Vinicius Kopcheski <viniciusfk@hotmail.com>
2022-09-05 22:26:52 +0200

    initial implementation of the worker
    still more code to be stolen from mrsequential.go

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -3,8 +3,10 @@ package mr
 import (
 	"fmt"
 	"hash/fnv"
+	"io/ioutil"
 	"log"
 	"net/rpc"
+	"os"
 )
 
 // Map functions return a slice of KeyValue.
@@ -25,26 +27,66 @@ func ihash(key string) int {
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
-	// Your worker implementation here.
+	// it gets two functions, the map and the reduce.
+	// how to decide which one to call?
+	// -> it depends on the coordinator response.
+	// -> if beginsWith pg*, map. otherwise, reduce.
+	// coord still does not know how to handle files create by the map function. How to inform it?
+	// via the request? Looks more efficient than polling the filesystem
+	// BUT, the coord will _have_ to poll the FS, as stated by the rules.
 
-	// uncomment to send the Example RPC to the coordinator.
-	CallExample()
+	var reply = RequestTask()
+
+	Map(reply, mapf)
+	// MAP impl:
+	// read the content of the file and send it as the 2nd arg, 1st must be ignored.
+	// it will return a KV pair
+	// write it to a file
+	// add it to the response.
+
+	// REDUCE impl:
+
+}
+
+func Map(fileName string, mapf func(string, string) []KeyValue) {
+	if len(fileName) == 0 {
+		return
+	}
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatalf("cannot open %v", fileName)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", fileName)
+	}
+	file.Close()
+	kva := mapf(fileName, string(content))
+
+	for key, element := range kva {
+		var nReduce = ihash(string(key)) % nReduceTasks
+	}
+
+	// A reasonable naming convention for intermediate files is mr-X-Y,
+	// where X is the Map task number, and Y is the reduce task number.
 
 }
 
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-func CallExample() {
+func RequestTask() string {
 
 	// declare an argument structure.
-	args := ExampleArgs{}
+	args := WorkerArgs{}
 
 	// fill in the argument(s).
-	args.X = 99
+	args.processedFileName = ""
+	args.workerName = ""
 
 	// declare a reply structure.
-	reply := ExampleReply{}
+	reply := FileNameReply{}
 
 	// send the RPC request, wait for the reply.
 	// the "Coordinator.Example" tells the
@@ -52,11 +94,13 @@ func CallExample() {
 	// the Example() method of struct Coordinator.
 	ok := call("Coordinator.Example", &args, &reply)
 	if ok {
-		// reply.Y should be 100.
-		fmt.Printf("reply.Y %v\n", reply.Y)
+		fmt.Printf("Got task %v to process.\n", reply.taskFileName)
+		return reply.taskFileName
 	} else {
 		fmt.Printf("call failed!\n")
+		return ""
 	}
+
 }
 
 // send an RPC request to the coordinator, wait for the response.
